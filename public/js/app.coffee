2fc6ba821f7dca7c3aa7e565a3f8e54f915449dc
Anders Fisher <atleastimtrying@gmail.com>
2011-12-12 16:46:22 +0000

    nicely looping through touches now

--- public/js/app.coffee
+++ public/js/app.coffee
@@ -1,2 +1,29 @@
+window.sketch = (p5) ->
+  p5.setup = ->
+    p5.colorMode p5.HSB, 300, 10, 10, 10
+    p5.size(window.innerWidth, window.innerHeight)
+    p5.smooth()
+    p5.background 0
+  p5.draw = ->
+    p5.noStroke()
+    p5.fill 0, 0, 0, 0.8
+    p5.rect 0, 0, p5.width, p5.height
+    p5.stroke 255
+
+window.drawdot = (touch, first)->
+  window.processing.ellipse touch.pageX, touch.pageY, 30, 30
+  window.processing.line touch.pageX, touch.pageY, first.pageX, first.pageY
+window.touches = (e)->
+  e.preventDefault();
+  window.drawdot touch, e.originalEvent.touches[0] for touch in e.originalEvent.touches
+
 $ ->
-  console.log 'hello world!'  
+  canvas = $("canvas")
+  window.processing = new Processing canvas[0], window.sketch
+  $(window).resize ->
+    $("canvas").css {
+      height : $(window).height()
+      width : $(window).width()
+    }
+    processing.size $(window).width(), $(window).height()
+  $("canvas").bind 'touchmove', window.touches
--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,46 @@
 
+  window.sketch = function(p5) {
+    p5.setup = function() {
+      p5.colorMode(p5.HSB, 300, 10, 10, 10);
+      p5.size(window.innerWidth, window.innerHeight);
+      p5.smooth();
+      return p5.background(0);
+    };
+    return p5.draw = function() {
+      p5.noStroke();
+      p5.fill(0, 0, 0, 0.8);
+      p5.rect(0, 0, p5.width, p5.height);
+      return p5.stroke(255);
+    };
+  };
+
+  window.drawdot = function(touch, first) {
+    window.processing.ellipse(touch.pageX, touch.pageY, 30, 30);
+    return window.processing.line(touch.pageX, touch.pageY, first.pageX, first.pageY);
+  };
+
+  window.touches = function(e) {
+    var touch, _i, _len, _ref, _results;
+    e.preventDefault();
+    _ref = e.originalEvent.touches;
+    _results = [];
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      touch = _ref[_i];
+      _results.push(window.drawdot(touch, e.originalEvent.touches[0]));
+    }
+    return _results;
+  };
+
   $(function() {
-    return console.log('hello world!');
+    var canvas;
+    canvas = $("canvas");
+    window.processing = new Processing(canvas[0], window.sketch);
+    $(window).resize(function() {
+      $("canvas").css({
+        height: $(window).height(),
+        width: $(window).width()
+      });
+      return processing.size($(window).width(), $(window).height());
+    });
+    return $("canvas").bind('touchmove', window.touches);
   });
